a3d5b0c9352760bd755a04b807b47e8671043f0b
HareeshKumaran-N <hareez531hk@gmail.com>
2024-01-20 19:23:53 +0530

    Release 1 disabled eslint for build

--- blog-app/next.config.js
+++ blog-app/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   images: {
     domains: ["www.vhv.rs", "firebasestorage.googleapis.com", "giphy.com"],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
